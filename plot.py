56e9345cdad1a3dd64f711f46492b07c7536fdfc
Prateek Sahay <ptkinvent@gmail.com>
2023-02-21 22:39:37 -0800

    Added command line argument for image for plot.py

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@
 Reference: https://realpython.com/python-opencv-color-spaces/
 '''
 
+import sys
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,19 +12,25 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib import colors
 
-img = cv2.imread('./data/DSC_5122.JPG')
+if len(sys.argv) != 2:
+    print('Usage: python3 main.py <fname>')
+    exit(0)
+fname = sys.argv[1]
+img = cv2.imread(fname)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # Resize smaller
-scale_percent = 5
-width = int(img.shape[1] * scale_percent/100)
-height = int(img.shape[0] * scale_percent/100)
+scale = 5/100
+width = int(img.shape[1] * scale)
+height = int(img.shape[0] * scale)
 dim = (width, height)
 img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
+# Plot RGB
 r, g, b = cv2.split(img)
-fig = plt.figure()
-axis = fig.add_subplot(1, 1, 1, projection="3d")
+fig_rgb = plt.figure()
+axis = fig_rgb.add_subplot(1, 1, 1, projection="3d")
+axis.set_title("RGB")
 
 pixel_colors = img.reshape((np.shape(img)[0]*np.shape(img)[1], 3))
 norm = colors.Normalize(vmin=-1.,vmax=1.)
@@ -34,29 +41,16 @@ axis.scatter(r.flatten(), g.flatten(), b.flatten(), facecolors=pixel_colors, mar
 axis.set_xlabel("Red")
 axis.set_ylabel("Green")
 axis.set_zlabel("Blue")
-# plt.show()
 
-
-
+# Plot HSV
 hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 h, s, v = cv2.split(hsv_img)
-fig = plt.figure()
-axis = fig.add_subplot(1, 1, 1, projection="3d")
+fig_hsv = plt.figure()
+axis = fig_hsv.add_subplot(1, 1, 1, projection="3d")
+axis.set_title("HSV")
 
 axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker=".")
 axis.set_xlabel("Hue")
 axis.set_ylabel("Saturation")
 axis.set_zlabel("Value")
-plt.show()
-
-
-light_orange = (12, 100, 0)
-dark_orange = (25, 255, 255)
-
-mask = cv2.inRange(hsv_img, light_orange, dark_orange)
-result = cv2.bitwise_and(img, img, mask=mask)
-plt.subplot(1, 2, 1)
-plt.imshow(mask, cmap="gray")
-plt.subplot(1, 2, 2)
-plt.imshow(result)
 plt.show()
